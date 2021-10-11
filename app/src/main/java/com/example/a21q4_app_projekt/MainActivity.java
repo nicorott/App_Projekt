27c76c35e0893fee6ba1dbc1c0325a1cfb85360f
FAue <florian.aue@edu.fhdw.de>
2021-10-11 17:31:29 +0200

    First Commit and Push

--- app/src/main/java/com/example/a21q4_app_projekt/MainActivity.java
+++ app/src/main/java/com/example/a21q4_app_projekt/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.a21q4_app_projekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
