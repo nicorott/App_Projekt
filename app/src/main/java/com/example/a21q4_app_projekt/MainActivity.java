795abc3ad0e462447a0d8af87639cd887d0e961c
FAue <florian.aue@edu.fhdw.de>
2021-10-11 17:30:19 +0200

    First Commit and Push

---
+++ app/src/main/java/com/example/a21q4_app_projekt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a21q4_app_projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
