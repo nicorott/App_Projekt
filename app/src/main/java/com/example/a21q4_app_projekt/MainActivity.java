8e7df2f3e6951c3726511ff8acb8ceb5bd5f970b
maxne_9nfgloq <max.neumann@edu.fhdw.de>
2021-10-22 16:00:07 +0200

    hhh

--- app/src/main/java/com/example/a21q4_app_projekt/MainActivity.java
+++ app/src/main/java/com/example/a21q4_app_projekt/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     EditText test;
     EditText test2;
     EditText test3;
+    EditText test4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
