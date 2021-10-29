9ec21e5385f298a042e0833839eaa11f82eef2c9
maxne_9nfgloq <max.neumann@edu.fhdw.de>
2021-10-29 15:54:35 +0200

    Test

--- app/src/main/java/activity/SignUpActivity.java
+++ app/src/main/java/activity/SignUpActivity.java
@@ -23,6 +23,7 @@ public class SignUpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 
     FirebaseAuth Auth = FirebaseAuth.getInstance();
